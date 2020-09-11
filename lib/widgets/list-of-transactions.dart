75818ce1c3a8ea28bdeba6d78e29fd587e81e7bb
Maksym Malyshko <maksymmalyshko@Maksyms-MacBook-Pro-2.local>
2020-09-11 17:55:32 +0300

    add ListView.builder to list of transactions

--- lib/widgets/list-of-transactions.dart
+++ lib/widgets/list-of-transactions.dart
@@ -11,44 +11,42 @@ class ListOfTransactions extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((t) {
-            return Card(
-                child: Row(
-              children: [
-                Container(
-                  width: 110,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey, width: 1)),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$${t.amount}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.black87),
-                  ),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                width: 110,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1)),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '\$${transactions[index].amount}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black87),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      t.title,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(t.date),
-                      style: TextStyle(fontSize: 12, color: Colors.black87),
-                    )
-                  ],
-                )
-              ],
-            ));
-          }).toList(),
-        ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transactions[index].title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                    style: TextStyle(fontSize: 12, color: Colors.black87),
+                  )
+                ],
+              )
+            ],
+          ));
+        },
+        itemCount: transactions.length,
       ),
     );
   }
